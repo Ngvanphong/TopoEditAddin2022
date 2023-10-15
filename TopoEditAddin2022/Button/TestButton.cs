2235c9a83840587538b3aa1bc7cbb57c463f8343
Nguyen van Phong <ngvanphong2012@gmail.com>
2023-10-15 08:11:07 +0700

    52525

--- TopoEditAddin2022/Button/TestButton.cs
+++ TopoEditAddin2022/Button/TestButton.cs
@@ -12,36 +12,36 @@ namespace TopoEditAddin2022.Button
 {
     public class TestButton
     {
-        public void Test(UIControlledApplication application)
-        {
-            try
+            public void Test(UIControlledApplication application)
             {
-                application.CreateRibbonTab(AppConstants.RibbonName1); // tao riboon neu ribbon da ton tai thi xuong ham catch
-            }
-            catch { }
-            RibbonPanel panelArchitect = null;
-            List<RibbonPanel> allPanelOfRevitAPI = application.GetRibbonPanels(AppConstants.RibbonName1); // de get toan bo panel cua ribbon
-            foreach (RibbonPanel panelItem in allPanelOfRevitAPI) // kiem tra panel da ton tai hay chua
-            {
-                if (panelItem.Name == AppConstants.Panel1)
+                try
                 {
-                    panelArchitect = panelItem;
-                    break;
+                    application.CreateRibbonTab(AppConstants.RibbonName1); // tao riboon neu ribbon da ton tai thi xuong ham catch
                 }
-            }
-            if (panelArchitect == null) // tao panel
-            {
-                panelArchitect = application.CreateRibbonPanel(AppConstants.RibbonName1, AppConstants.Panel1); // tao panel
-            }
+                catch { }
+                RibbonPanel panelArchitect = null;
+                List<RibbonPanel> allPanelOfRevitAPI = application.GetRibbonPanels(AppConstants.RibbonName1); // de get toan bo panel cua ribbon
+                foreach (RibbonPanel panelItem in allPanelOfRevitAPI) // kiem tra panel da ton tai hay chua
+                {
+                    if (panelItem.Name == AppConstants.Panel1)
+                    {
+                        panelArchitect = panelItem;
+                        break;
+                    }
+                }
+                if (panelArchitect == null) // tao panel
+                {
+                    panelArchitect = application.CreateRibbonPanel(AppConstants.RibbonName1, AppConstants.Panel1); // tao panel
+                }
 
-            ImageSource imageSource = Extension.GetImageSource(Resources.Copy);
-            PushButtonData testButton = new PushButtonData("TestAddin", "Test \n Function",
-                Assembly.GetExecutingAssembly().Location, "TopoEditAddin2022.TestBiding.TestBinding");
-            testButton.ToolTip = "Test button";
-            testButton.LongDescription = "Test button";
-            testButton.Image = imageSource;
-            testButton.LargeImage = imageSource;
-            panelArchitect.AddItem(testButton).Enabled = true;
-        }
+                ImageSource imageSource = Extension.GetImageSource(Resources.Copy);
+                PushButtonData testButton = new PushButtonData("TestAddin", "Test \n Function",
+                    Assembly.GetExecutingAssembly().Location, "TopoEditAddin2022.TestBiding.TestBinding");
+                testButton.ToolTip = "Test button";
+                testButton.LongDescription = "Test button";
+                testButton.Image = imageSource;
+                testButton.LargeImage = imageSource;
+                panelArchitect.AddItem(testButton).Enabled = true;
+            }
     }
 }
